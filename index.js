e0b7cbc8c26108106da6fbff035ac2196164e39d
nicholas koyi <koyinicholas@gmail.com>
2024-03-27 21:22:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arrRecords) {
+  // define the a variable that contains the year won
+  let yearWon;
+  arrRecords.find((arrRecord) => {
+    if (arrRecord.result === "W") {
+      yearWon = arrRecord.year;
+    }
+    return yearWon;
+  });
+  return yearWon;
+}
